595e718f3673f377950de0a25d7698efebb36148
yefeng <yefeng.freedom@gmail.com>
2017-02-20 17:43:54 +0800

    add before and after

--- gintonic/src/main/java/org/android10/gintonic/aspect/TraceAspect.java
+++ gintonic/src/main/java/org/android10/gintonic/aspect/TraceAspect.java
@@ -8,8 +8,10 @@ package org.android10.gintonic.aspect;
 import android.util.Log;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
 /**
@@ -21,57 +23,67 @@ public class TraceAspect {
 //    private static final String POINTCUT_METHOD =
 //            "execution(* *.*(..))";
 
-  //    private static final String POINTCUT_METHOD =
+    //    private static final String POINTCUT_METHOD =
 //            "execution(public void org.android10.viewgroupperformance.activity.MainActivity.test(..))";
-  private static final String POINTCUT_METHOD =
-          "execution(public void android.view.View.OnClickListener.onClick(..))";
+    private static final String POINTCUT_METHOD =
+            "execution(public void android.view.View.OnClickListener.onClick(..))";
 
 
-  private static final String POINTCUT_CONSTRUCTOR =
-          "execution(@org.android10.gintonic.annotation.DebugTrace *.new(..))";
+    private static final String POINTCUT_CONSTRUCTOR =
+            "execution(@org.android10.gintonic.annotation.DebugTrace *.new(..))";
 
-  @Pointcut(POINTCUT_METHOD)
-  public void methodAnnotatedWithDebugTrace() {
-  }
+    @Pointcut(POINTCUT_METHOD)
+    public void methodAnnotatedWithDebugTrace() {
+    }
 
-  @Pointcut(POINTCUT_CONSTRUCTOR)
-  public void constructorAnnotatedDebugTrace() {
-  }
+    @Pointcut(POINTCUT_CONSTRUCTOR)
+    public void constructorAnnotatedDebugTrace() {
+    }
 
-  @Around("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
-  public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
+    public Object weaveJoinPoint(ProceedingJoinPoint joinPoint) throws Throwable {
 //        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
 //        String className = methodSignature.getDeclaringType().getSimpleName();
 //        String methodName = methodSignature.getName();
-    Log.e("do it", "do it before");
+        Log.e("do it", "do it before");
 //        final StopWatch stopWatch = new StopWatch();
 //        stopWatch.start();
-    Object result = joinPoint.proceed();
-    Log.e("do it", "do it after");
+        Object result = joinPoint.proceed();
+        Log.e("do it", "do it after");
 //        stopWatch.stop();
 //
 //        DebugLog.log(className, buildLogMessage(methodName, stopWatch.getTotalTimeMillis()));
 
-    return result;
-  }
+        return result;
+    }
 
-  /**
-   * Create a log message.
-   *
-   * @param methodName     A string with the method name.
-   * @param methodDuration Duration of the method in milliseconds.
-   * @return A string representing message.
-   */
-  private static String buildLogMessage(String methodName, long methodDuration) {
-    StringBuilder message = new StringBuilder();
-    message.append("Gintonic --> ");
-    message.append(methodName);
-    message.append(" --> ");
-    message.append("[");
-    message.append(methodDuration);
-    message.append("ms");
-    message.append("]");
+    @Before("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
+    public void before(ProceedingJoinPoint joinPoint) {
+        Log.e("do it", "before");
+    }
 
-    return message.toString();
-  }
+    @After("methodAnnotatedWithDebugTrace() || constructorAnnotatedDebugTrace()")
+    public void after(ProceedingJoinPoint joinPoint) {
+        Log.e("do it", "after");
+    }
+
+    /**
+     * Create a log message.
+     *
+     * @param methodName     A string with the method name.
+     * @param methodDuration Duration of the method in milliseconds.
+     * @return A string representing message.
+     */
+    private static String buildLogMessage(String methodName, long methodDuration) {
+        StringBuilder message = new StringBuilder();
+        message.append("Gintonic --> ");
+        message.append(methodName);
+        message.append(" --> ");
+        message.append("[");
+        message.append(methodDuration);
+        message.append("ms");
+        message.append("]");
+
+        return message.toString();
+    }
 }
